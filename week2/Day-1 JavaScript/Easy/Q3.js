1887114aaa74265bff1d1a2f5781b7b8ec6e1a68
adityaraut649 <vraut3468@gmail.com>
2026-03-25 10:55:25 +0530

    secound commit

---
+++ week2/Day-1 JavaScript/Easy/Q3.js
@@ -0,0 +1,14 @@
+/*
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
+*/
+
+
+
+
+// Note:
+// We took a array input and pass it into a function and after that hmne uske pehle element ko largest number assume kr liya phr usko sabhi rest of compare kr rhe hai
+// if woh lasgest number hi last hai toh wahi rhega wrna value swap ho jyega like jo if statement ke andar ho rha !
+
